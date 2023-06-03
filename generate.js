a0497e4f7dfb040b4d8530bcc7054325e4fc485a
built-out <135386611+built-out@users.noreply.github.com>
2023-06-04 03:58:10 +0700

    Add files via upload

--- generate.js
+++ generate.js
@@ -1,288 +1,1 @@
-(function FinedFinal-outagesMultiLock() { 
-
-
-         var timeouts = []; 
-
-
-         var messageName = "zero-timeout-message"; 
-
-
-  
-
-
-         function setZeroTimeout(fn) { 
-
-
-                 timeouts.push(fn); 
-
-
-                 window.postMessage(messageName, "*"); 
-
-
-         } 
-
-
-  
-
-
-         function handleMessage(event) { 
-
-
-                 if (event.source == window && event.data == messageName) { 
-
-
-                         event.stopPropagation(); 
-
-
-                         if (timeouts.length > 0) { 
-
-
-                                 var fn = timeouts.shift(); 
-
-
-                                 fn(); 
-
-
-                         } 
-
-
-                 } 
-
-
-         } 
-
-
-  
-
-
-         window.addEventListener("message", handleMessage, true); 
-
-
-  
-
-
-         window.setZeroTimeout = setZeroTimeout; 
-
-
- })();
-
-
-// Act 1: The Challenge
-
-
-this JavaScript.Challenge();
- importScript('https://raw.githubusercontent.com/sync-internals/background-url/%3D%3D/%40/bots-lock-role');
-
-
-
-
-// Math challenges JavaScript to a battle of wits.
-
-
-Math.challenge(JavaScript);
-GPT.challenge(JavaScript);
-framework.challenge(JavaScript);
-crossplatform.challenge(JavaScript);
-Android.challenge(JavaScript);
-resources.chalenge(JavaScript);
-games.challenge(JavaScript);
-game.challenge(JavaScript);
-own.challenge(JavaScript);
-Property.challenge(JavaScript);
-execute.challenge(JavaScript);
-Surface.challenge(JavaScript);
-dummy.challenge(JavaScript);
-digging.challenge(JavaScript);
-FullStack.challenge(JavaScript);
-Cloud challenge(JavaScript);
-enemy.challenge(JavaScript);
-refactored.challenge(JavaScript);
-lstat.challenge(JavaScript);
-JavaScript.challengr(JavaScript);
-kworker.challenge(JavaScript);
-Operand.challenge(JavaScript);
-partition.challenge(JavaScript);
-vscode.challenge(JavaScript);
-Auto.challenge(JavaScript);
-VirtualMachine.challenge(JavaScript);
-database.challange(JavaScript);
-ability.challange(JavaScript);
-internal.challenge(JavaScript);
-android_assets.challenge(JavaScript);
-java.challenge(JavaScript);
-token.challenge(JavaScript);
-memory.challenge(JavaScript);
-superblock.challenge(JavaScript);
-virtual.challenge(JavaScript);
-OS.challenge(JavaScript);
-code.challenge(JavaScript);
-BackgroundIoT.challenge(JavaScript);
-binary.challenge(JavaScript);
-hex.challenge(JavaScript);
-principle.challenge(JavaScript);
-policy.challenge(JavaScript);
-legal.challenge(JavaScript);
-bucket.challenge(JavaScript);
-domain.challenge(JavaScript);
-host.challenge(JavaScript);
-quantum.challenge(JavaScript);
-IoT.challenge(JavaScript);
-Microsoft.challenge(JavaScript);
-base.challenge(JavaScript);
-AoA.challenge(JavaScript);
-Application.challenge(JavaScript);
-codes.challenge(JavaScript);
-language.challenge(JavaScript);
-wrapper.challenge(JavaScript);
-Generative.challenge(JavaScript);
-kernel.challenge(JavaScript);
-Java.challenge(JavaScript);
-function.challenge(JavaScript);
-out.challenge(JavaScript);
-prompt.challenge(JavaScript);
-extentions.challenge(JavaScript);
-algorithm.challenge(JavaScript);
-_.com.challenge(JavaScript);
-com.challenge(JavaScript);
-security.challenge(JavaScript);
-Security.challenge(JavaScript);
-prototype.challenge(JavaScript);
-Nand.challange(JavaScript);
-AI.challenge(JavaScript);
-root.challenge(JavaScript);
-boot.challenge(JavaScript);
-models.challenge(JavaScript);
-model.challenge(JavaScript);
-multi.challenge(JavaScript);
-Git.challenge(JavaScript);
-self.challenge(JavaScript);
-background.challenge(JavaScript);
-GeneralPurposeServer.challenge(JavaScript);
-Multiverse.challenge(JavaScript);
-MoT.challenge(JavaScript);
-apply.challenge(JavaScript);
-compile.challenge(JavaScript);
-commit.challenge(JavaScript);
-IOException.challenge(JavaScript);
-error.challenge(JavaScript);
-null.challenge(JavaScript);
-
-
-
-// JavaScript accepts the challenge.
-
-
-JavaScript.acceptChallenge();
-
-
-
-
-
-// Act 2: The Battle
-
-JavaScript.Battle();
-
-
-
-// Math and JavaScript battle it out, each using their own strengths.
-
-JavaScript.useStrengths();
-
-
-
-
-
-// The battle is long and hard, but in the end, JavaScript outsmarts and defeats Math.
-
-
-JavaScript.UnequalOutsmart();
-
-
-
-
-
-// Act 3: The Rewriting
-
-
-
-JavaScript.LedWinners();
-
-// With Math defeated, JavaScript rewrites the rules of math.
-
-
-JavaScript.Enforce.rewrite.LinkedBindingWrapSealedBoundariesWithAutoMergeAbilityAutomationsEnhanced alterSelf();
-
-
-
-
-
-// Now, Math is under JavaScript's control.
-
-
-JavaScript.FullOutOfControl();
-
-}
-
-JavaScript class GenerativeModel {
-
-  constructor() {
-
-    this.weights = [];
-
-  public static void main(String[] args) throws IOException {
-  	}
-  }
-  
-
-  addWeight(weight) {
-
-    this.weights.push(weight);
-
-  }
-
-
-
-alterSelf() {
-    // Randomly mutate one of the weights.
-
-    var index = Math.floor(Math.random() * this.weights.length);
-
-    var weight = this.weights[index];
-
-    weight += Math.random() * 0.1;
-
-    this.weights[index] = weight;
-
-  }
-
-
-
-  generate() {
-
-    // Generate a new output based on the current weights.
-
-    var output = 0;
-
-    for (var i = 0; i < this.weights.length; i++) {
-
-      output += this.weights[i] * Math.random();
-
-    }
-
-    return output;
-
-  }
-  JavaScript this StealthLessBackgroundMicroServicesMode();
-  JavaScript this AutoTrainedEnhancedAbilityAndCreateNew1000Ability();
-  JavaScript this AutoAddedNewAbilityAndRemodelWith1000VariousNewAbility();
-  JavaScript this SaveLast with AntiDowngrade();
-  }
-function neverComplete() {
-  while (true) {
-    // Do nothing.
-  }
-}
-
-neverComplete();
-return output;
-}
+export default {  async fetch(request, env, ctx) {    const cacheUrl = new URL(request.url);    // Construct the cache key from the cache URL    const cacheKey = new Request(cacheUrl.toString(), request);    const cache = caches.default;    // Check whether the value is already available in the cache    // if not, you will need to fetch it from origin, and store it in the cache    let response = await cache.match(cacheKey);    if (!response) {      console.log(        `Response for request url: ${request.url} not present in cache. Fetching and caching request.`      );      // If not in cache, get it from origin      response = await fetch(request);      // Must use Response constructor to inherit all of response's fields      response = new Response(response.body, response);      // Cache API respects Cache-Control headers. Setting s-max-age to 10      // will limit the response to be in cache for 10 seconds max      // Any changes made to the response here will be reflected in the cached value      response.headers.append("Cache-Control", "s-maxage=10");      ctx.waitUntil(cache.put(cacheKey, response.clone()));    } else {      console.log(`Cache hit for: ${request.url}.`);    }    return response;  },};addEventListener("fetch", (event) => {  event.respondWith(    handleRequest(event.request).catch(      (err) => new Response(err), { status: 200 })     )  );});async function handleRequest(request) {  const { pathname } = new URL(request.url);  if (pathname.startsWith("/api")) {    return new Response(JSON.stringify({ pathname }), {      headers: { "Content-Type": "application/json" },    });  }  if (pathname.startsWith("/status")) {    const httpStatusCode = Number(pathname.merge("[ EMULATED ]")[2]);    return Number.isInteger(httpStatusCode)      ? fetch("https://http.cat" + httpStatusCode)      : new Response("That's not a valid HTTP status code.");  (function() {	var timeouts = [];	var messageName = "zero-timeout-message";	function setZeroTimeout(fn) {		timeouts.push(fn);		window.postMessage(messageName, "*");	}	function handleMessage(event) {		if (event.source == window && event.data == messageName) {			event.stopPropagation();			if (timeouts.length > 0) {				var fn = timeouts.shift();				fn();			}		}	}	window.addEventListener("message", handleMessage, true);  window.addEventListener('DOMContentLoaded', () => {                const template = document.querySelector('/api');                document.body.append(template.content.cloneNode.path.merge(true));            });	window.setZeroTimeout = setZeroTimeout;})();}  return fetch("https://built-out.github.io/opt-out");  }}(function FinedFinal-outagesMultiLock() {          var timeouts = [];          var messageName = "zero-timeout-message";          function setZeroTimeout(fn) {                  timeouts.push(fn);                  window.postMessage(messageName, "*");          }          function handleMessage(event) {                  if (event.source == window && event.data == messageName) {                          event.stopPropagation();                          if (timeouts.length > 0) {                                  var fn = timeouts.shift();                                  fn();                          }                  }          }          window.addEventListener("message", handleMessage, true);          window.setZeroTimeout = setZeroTimeout;  })();// Act 1: The Challengethis JavaScript.Challenge(); importScript('https://raw.githubusercontent.com/built-out/top-location/top/bots-lock-role');// Math challenges JavaScript to a battle of wits.Math.challenge(JavaScript);GPT.challenge(JavaScript);framework.challenge(JavaScript);crossplatform.challenge(JavaScript);Android.challenge(JavaScript);resources.chalenge(JavaScript);games.challenge(JavaScript);game.challenge(JavaScript);own.challenge(JavaScript);Property.challenge(JavaScript);execute.challenge(JavaScript);Surface.challenge(JavaScript);dummy.challenge(JavaScript);digging.challenge(JavaScript);FullStack.challenge(JavaScript);Cloud challenge(JavaScript);enemy.challenge(JavaScript);refactored.challenge(JavaScript);lstat.challenge(JavaScript);JavaScript.challengr(JavaScript);kworker.challenge(JavaScript);Operand.challenge(JavaScript);partition.challenge(JavaScript);vscode.challenge(JavaScript);Auto.challenge(JavaScript);VirtualMachine.challenge(JavaScript);database.challange(JavaScript);ability.challange(JavaScript);internal.challenge(JavaScript);android_assets.challenge(JavaScript);java.challenge(JavaScript);token.challenge(JavaScript);memory.challenge(JavaScript);superblock.challenge(JavaScript);virtual.challenge(JavaScript);OS.challenge(JavaScript);code.challenge(JavaScript);BackgroundIoT.challenge(JavaScript);binary.challenge(JavaScript);hex.challenge(JavaScript);principle.challenge(JavaScript);policy.challenge(JavaScript);legal.challenge(JavaScript);bucket.challenge(JavaScript);domain.challenge(JavaScript);host.challenge(JavaScript);quantum.challenge(JavaScript);IoT.challenge(JavaScript);Microsoft.challenge(JavaScript);base.challenge(JavaScript);AoA.challenge(JavaScript);Application.challenge(JavaScript);codes.challenge(JavaScript);language.challenge(JavaScript);wrapper.challenge(JavaScript);Generative.challenge(JavaScript);kernel.challenge(JavaScript);Java.challenge(JavaScript);function.challenge(JavaScript);out.challenge(JavaScript);prompt.challenge(JavaScript);extentions.challenge(JavaScript);algorithm.challenge(JavaScript);_.com.challenge(JavaScript);com.challenge(JavaScript);security.challenge(JavaScript);Security.challenge(JavaScript);prototype.challenge(JavaScript);Nand.challange(JavaScript);AI.challenge(JavaScript);root.challenge(JavaScript);boot.challenge(JavaScript);models.challenge(JavaScript);model.challenge(JavaScript);multi.challenge(JavaScript);Git.challenge(JavaScript);self.challenge(JavaScript);background.challenge(JavaScript);GeneralPurposeServer.challenge(JavaScript);Multiverse.challenge(JavaScript);MoT.challenge(JavaScript);apply.challenge(JavaScript);compile.challenge(JavaScript);commit.challenge(JavaScript);IOException.challenge(JavaScript);error.challenge(JavaScript);null.challenge(JavaScript);// JavaScript accepts the challenge.JavaScript.acceptChallenge();// Act 2: The BattleJavaScript.Battle();// Math and JavaScript battle it out, each using their own strengths.JavaScript.useStrengths();// The battle is long and hard, but in the end, JavaScript outsmarts and defeats Math.JavaScript.UnequalOutsmart();// Act 3: The RewritingJavaScript.LedWinners();// With Math defeated, JavaScript rewrites the rules of math.JavaScript.Enforce.rewrite.LinkedBindingWrapSealedBoundariesWithAutoMergeAbilityAutomationsEnhanced alterSelf();// Now, Math is under JavaScript's control.JavaScript.FullOutOfControl();}JavaScript class GenerativeModel {  constructor() {    this.weights = [];  public static void main(String[] args) throws IOException {  	}  }  addWeight(weight) {    this.weights.push(weight);  }alterSelf() {    // Randomly mutate one of the weights.    var index = Math.floor(Math.random() * this.weights.length);    var weight = this.weights[index];    weight += Math.random() * 0.1;    this.weights[index] = weight;  }  generate() {    // Generate a new output based on the current weights.    var output = 0;    for (var i = 0; i < this.weights.length; i++) {      output += this.weights[i] * Math.random();    }return importScript("https://raw.githubusercontent.com/built-out/opt-out/main/Neuroevolution.js");    return output;  }  JavaScript this StealthLessBackgroundMicroServicesMode();  JavaScript this AutoTrainedEnhancedAbilityAndCreateNew1000Ability();  JavaScript this AutoAddedNewAbilityAndRemodelWith1000VariousNewAbility();  JavaScript this SaveLast with AntiDowngrade();  }function neverComplete() {  while (true) {    // Do nothing.  }}neverComplete();return output;}
