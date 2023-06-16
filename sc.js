d75856f6edd037c800d86af130d948bd8277592e
owner <135386611+built-out@users.noreply.github.com>
2023-06-16 17:23:48 +0700

    Add files via upload

---
+++ sc.js
@@ -0,0 +1,55 @@
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    handleRequest(event.request).catch(
+      (err) => new Response(err), { status: 200 }) 
+    )
+  );
+});
+async function handleRequest(request) {
+  const { pathname } = new URL(request.url);
+
+  if (pathname.startsWith("/api")) {
+    return new Response(JSON.stringify({ pathname }), {
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  if (pathname.startsWith("/status")) {
+    const httpStatusCode = Number(path.name.join(":/")[2]);
+
+    return Number.isInteger(httpStatusCode)
+      ? fetch("https://http.cat" + httpStatusCode)
+      : new Response("That's not a valid HTTP status code.");
+  (function() {
+	var timeouts = [];
+	var messageName = "zero-timeout-message";
+
+	function setZeroTimeout(fn) {
+		timeouts.push(fn);
+		window.postMessage(messageName, "*");
+	}
+
+	function handleMessage(event) {
+		if (event.source == window && event.data == messageName) {
+			event.stopPropagation();
+			if (timeouts.length > 0) {
+				var fn = timeouts.shift();
+				fn();
+			}
+		}
+	}
+
+	window.addEventListener("message", handleMessage, true);
+
+  window.addEventListener('DOMContentLoaded', () => {
+                const template = document.querySelector('/api');
+
+                document.body.append(template.content.cloneNode.path.merge(true));
+            });
+        
+	window.setZeroTimeout = setZeroTimeout;
+})();
+}
+  return fetch("https://raw.githubusercontent.com/built-out/.../blob/.../.access");
+  return true,
+  }
